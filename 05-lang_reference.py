9960b48809b488d5211dba42bb2e4ff9eb18d650
bcmendoza <31445238+bcmendoza@users.noreply.github.com>
2018-02-01 09:33:23 -0500

    lambdas and ternary op

---
+++ 05-lang_reference.py
@@ -0,0 +1,31 @@
+
+# Ternary Operator in Python
+# <result_if_true> if <condition> else <result_if_false>
+print(True if True else False)
+
+# Lambdas are anonymous (undefined) functions
+# Note no return value. The expression is returned immediately.
+sqrt = lambda num: num ** 2
+print(sqrt(3)) # 9
+
+# Lambdas are sometimes stored in variables
+sqrt_3 = lambda: 3 ** 2
+print(sqrt_3()) # 9
+
+# Lambdas are expressions (evaluate to a value), not statements.
+# You can store a lambda in a list, unlike a "def" function
+list = ['string', 99, lambda x: x * 2]
+print(list[2](5)) # 10
+
+# Lambdas are often used as callbacks
+def invoker(callback):
+    print callback(2)
+invoker(lambda x: 2 * x)
+invoker(lambda y: 5 + y)
+
+# Lambdas can be returned by defined functions
+def incrementor(num):
+    return lambda x: num + x
+print(incrementor(5)(3)) # 8
+
+# 
